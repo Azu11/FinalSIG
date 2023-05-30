c8a6bec71bf19c535ff7fedaa4ff420e4029db71
Azu11 <angieortiz865@gmail.com>
2023-05-29 21:18:00 -0500

    historial-tittle-leyend

--- src/routes/historial.js
+++ src/routes/historial.js
@@ -3,10 +3,21 @@ const pg = require("../database");
 
 historialRouter.get("/", (req, res) => {
   const query = `
-    SELECT
-        *
-    FROM
-        caso_violencia 
+  SELECT
+    historial.id_historial as id,
+    historial.username,
+    caso_violencia.departamen as departament,
+    caso_violencia.municipio as municipio,  
+    caso_violencia.sexo as sexo,
+    caso_violencia.ocupacion as ocupacion,
+    caso_violencia.calidad_victima as calidad_victima,
+    caso_violencia.situacion as situacion
+  FROM
+    historial
+  INNER JOIN
+    caso_violencia
+  ON
+    historial.fk_caso = caso_violencia.gid
   ;`;
 
   pg.query(query, (err, rows, fields) => {
@@ -20,4 +31,57 @@ historialRouter.get("/", (req, res) => {
   });
 });
 
+historialRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const query = `
+      DELETE FROM historial WHERE id_historial = \$1;
+    `;
+  pg.query(query, [id], (err, result) => {
+    if (!err) {
+      console.log(`Registro con ID ${id} eliminado`);
+      res.sendStatus(204); 
+    } else {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+});
+
+historialRouter.post("/", (req, res) => {
+  const { id_usuario, username, fk_join } = req.body;
+
+  const getMaxIDQuery = "SELECT MAX(id_historial) AS lastID FROM historial";
+  pg.query(getMaxIDQuery, (error, results) => {
+    if (!error) {
+      const lastID = results.rows[0].lastid;
+      const newID = lastID ? lastID + 1 : 1;
+
+      const insertQuery = `
+      INSERT INTO historial (
+        id_historial,
+        username,
+        fk_caso
+      ) VALUES (\\$1, \\$2, \\$3)
+    `;
+    
+      const insertParams = [newID, username, fk_join];
+      pg.query(insertQuery, insertParams, (error, results) => {
+        if (!error) {
+          console.log(
+            `Registro insertado con id ${newID}, username ${username} y fk_caso ${fk_join}`
+          );
+          res.sendStatus(204); // Respuesta sin contenido
+        } else {
+          console.log(error);
+          res.status(500).json({ msg: "Error en la Base de Datos" });
+        }
+      });
+    } else {
+      console.log(error);
+      res.status(500).json({ msg: "Error de servidor" });
+    }
+  });
+});
+
 module.exports = historialRouter;
+si
